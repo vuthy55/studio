62909c938d0fd10ce4f31ab1d5cfee284a182067
Vuthy Taing <thegreenhomecommunity@gmail.com>
2025-07-24 07:56:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -35,10 +35,12 @@ export default function MainHeader({ title, description }: MainHeaderProps) {
                      <TooltipProvider>
                         <Tooltip>
                             <TooltipTrigger asChild>
-                                <Link href="/profile">
-                                    <Button variant="ghost" size="icon">
-                                        <User className="h-5 w-5" />
-                                        <span className="sr-only">My Account</span>
+                                <Link href="/profile" passHref legacyBehavior>
+                                    <Button asChild variant="ghost" size="icon">
+                                        <a>
+                                            <User className="h-5 w-5" />
+                                            <span className="sr-only">My Account</span>
+                                        </a>
                                     </Button>
                                 </Link>
                             </TooltipTrigger>
