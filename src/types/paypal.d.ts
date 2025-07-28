0e76d614aea6f65c2bfbe5ef2fe996c4d7567fc2
Vuthy Taing <thegreenhomecommunity@gmail.com>
2025-07-28 15:12:32 +0000

    proceed

---
+++ src/types/paypal.d.ts
@@ -0,0 +1,5 @@
+// This file provides a type declaration for the '@paypal/checkout-server-sdk' module,
+// which is a JavaScript module without its own built-in TypeScript types.
+// This declaration prevents TypeScript from throwing an error during the build process.
+
+declare module '@paypal/checkout-server-sdk';
