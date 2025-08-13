0b725823da5a0ec5cc3c16d8ea0861ac58544b1c
Vuthy Taing <thegreenhomecommunity@gmail.com>
2025-08-13 21:12:31 +0000

    i do not see any red banner. also i see that service worker is no longer

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 
 import type {NextConfig} from 'next';
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -43,4 +38,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
