e6b5338addff797f65d4eb8cb147be20c61d270f
Vuthy Taing <thegreenhomecommunity@gmail.com>
2025-08-14 09:19:20 +0000

    do not code explain build errors
    Log viewer toolbar
    starting build "61

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '@/app/globals.css';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { Toaster } from '@/components/ui/toaster';
 import ClientSidebar from '@/components/layout/client-sidebar';
