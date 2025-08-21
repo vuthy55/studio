78a693cec21fbea0e7cac3c6d033d6927708e2aa
Vuthy Taing <thegreenhomecommunity@gmail.com>
2025-08-21 00:48:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
@@ -167,7 +166,6 @@ export function AppSidebar() {
       <SidebarHeader>
         <Link href="/" className="font-headline text-2xl font-bold text-primary" onClick={closeSidebar}>
           VibeSync
-          <sup className="text-xs font-bold text-black ml-1">Beta</sup>
         </Link>
       </SidebarHeader>
       <SidebarContent>
